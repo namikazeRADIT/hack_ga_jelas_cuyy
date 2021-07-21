7b26433bba41c169f78b8aca25f67c5498e0d71f
namikazeRADIT <87743119+namikazeRADIT@users.noreply.github.com>
2021-07-21 10:19:06 +0700

    Add files via upload
    pkg install git
    
    pkg install figlet
    
    git clone https://github.com/namikazeRADIT/hack_ga_jelas_cuyy

---
+++ hack_ga_jelas_cuyy.sh
@@ -0,0 +1,34 @@
+sleep 1
+echo 
+sleep 1
+figlet HACK GA JELAS
+echo 'By Radit Gemink'
+echo
+echo 'selamat datang di hack ga jelas dan dah itu aja kan aku udah dah bilang'
+echo
+echo '[1] Hacker Gj'
+echo '[2] exit'
+echo
+read -p 'Pilih Tools Gj ====> ' pill
+
+#data masukan
+if [ $pill = 1 ]; then
+echo 'hacker termux https ga jelas'
+pkg update && pkg upgrade
+echo 'Beress Coyy'
+echo 'Loading Ngab...'
+sleep 1
+#tampilan 2
+figlet HACK GAJELAS
+echo termux hack ga jelas
+echo hack
+echo termux hack
+echo
+sleep 1
+fi
+
+#data 2
+if [ $pill = 2 ]; then
+echo 'goodbye ngab :y padahal ga tau arti ngab :v'
+exit
+fi
